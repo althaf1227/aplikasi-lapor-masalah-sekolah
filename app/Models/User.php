a2745459ec6b2988271d2790026323756153c597
althaf <althafntok237@gmail.com>
2025-03-03 01:26:36 +0700

    Membuat Halaman Login Berserta logic Login

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,9 +17,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'nama',
+        'nis',
+        'code_guru',
         'password',
+        'role',
     ];
 
     /**
@@ -44,4 +46,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function username()
+    {
+        return 'login';
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthLoginController;
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::get('/', [AuthLoginController::class, 'showLogin'])->name('login');
+Route::post('/', [AuthLoginController::class, 'login']);
+Route::get('/logout', [AuthLoginController::class, 'logout'])->name('logout');
+
+
+Route::get('/siswa/dashboard', function () {
+    return view('SiswaPage/DashboardSiswa');
+})->middleware('auth')->name('siswa.dashboard');
+
+Route::middleware(['auth', 'role:guru'])->group(function () {
+    Route::get('/guru/dashboard', function () {
+        return view('GuruPage/DashboardGuru');
+    })->name('guru.dashboard');
 });
